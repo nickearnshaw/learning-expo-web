6577fba0ae380e4aaa23dfc8e9fda3acd0a8e42b
Nick Earnshaw <nick@earnshaw.org.uk>
2024-07-14 17:54:45 +0000

    Lifst state using functional component

--- my-app/components/WeatherContainer.tsx
+++ my-app/components/WeatherContainer.tsx
@@ -1,65 +1,65 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Weather from './Weather';
 
 export const API_KEY = '0dad96516f509d333f7ef4f79a2a3802';
 
-export default class weatherContainer extends Component {
-  state = {
+const WeatherContainer = () => {
+  const [state, setState] = useState({
     isLoading: true,
     temperature: 0,
-    error: null
-  };
+    error: null,
+  });
 
-  componentDidMount() {
+  useEffect(() => {
+    const fetchWeather = (lat = 25, lon = 25) => {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      )
+        .then(res => res.json())
+        .then(json => {
+          setState({
+            temperature: json.main.temp,
+            isLoading: false,
+            error: null,
+          });
+        })
+        .catch(error => {
+          setState(s => ({ ...s, error: 'Error Getting Weather Conditions' }));
+        });
+    };
+
     navigator.geolocation.getCurrentPosition(
       position => {
-        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+        fetchWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
-        this.setState({
-          error: 'Error Getting Weather Condtions'
-        });
+        setState(s => ({ ...s, error: 'Error Getting Weather Conditions' }));
       }
     );
-  }
+  }, []); // Empty dependency array means this effect runs once on mount
 
-  fetchWeather(lat = 25, lon = 25) {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-    )
-      .then(res => res.json())
-      .then(json => {
-        console.log(json);
-        this.setState({
-          temperature: json.main.temp,
-          isLoading: false,
-        });
-      });
-  }
+  const { isLoading, temperature, error } = state;
 
-
-  render() {
-    const { isLoading, temperature } = this.state;
-    return (
-      <View style={styles.container}>
-        {isLoading ? (
-          <View>
-            <Text>Loading</Text>
-          </View>
-        ) : (
-          <Weather temperature={temperature} />
-        )}
-      </View>
-    );
-  }
-}
+  return (
+    <View style={styles.container}>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : error ? (
+        <Text>{error}</Text>
+      ) : (
+        <Weather temperature={temperature} />
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center'
-  }
+  },
 });
+
+export default WeatherContainer;
